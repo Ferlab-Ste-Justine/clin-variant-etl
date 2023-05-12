fd62862a6994c36db89c387e56ec826a8e526f74
Laura Bégin <lbegin@ferlab.bio>
2023-05-12 14:44:33 -0400

    refactor: CLIN-1949 Extract exomiser files using FileUtils

--- src/main/scala/bio/ferlab/clin/etl/normalized/Exomiser.scala
+++ src/main/scala/bio/ferlab/clin/etl/normalized/Exomiser.scala
@@ -1,10 +1,10 @@
 package bio.ferlab.clin.etl.normalized
 
+import bio.ferlab.clin.etl.utils.FileUtils
 import bio.ferlab.datalake.commons.config.{Configuration, DatasetConf}
 import bio.ferlab.datalake.spark3.etl.ETLSingleDestination
-import bio.ferlab.datalake.spark3.implicits.DatasetConfImplicits.DatasetConfOperations
 import bio.ferlab.datalake.spark3.transformation.Cast.{castFloat, castInt, castLong}
-import org.apache.spark.sql.functions.{input_file_name, split, substring_index}
+import org.apache.spark.sql.functions.input_file_name
 import org.apache.spark.sql.types.BooleanType
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
@@ -14,11 +14,17 @@ class Exomiser(batchId: String)(implicit configuration: Configuration) extends E
   override val mainDestination: DatasetConf = conf.getDataset("normalized_exomiser")
   val raw_exomiser: DatasetConf = conf.getDataset("raw_exomiser")
 
-
   override def extract(lastRunDateTime: LocalDateTime,
                        currentRunDateTime: LocalDateTime)(implicit spark: SparkSession): Map[String, DataFrame] = {
+    import spark.implicits._
+    val exomiserFiles = FileUtils.filesUrl(batchId, "EXOMISER", "TSV")
+
     Map(
-      raw_exomiser.id -> raw_exomiser.copy(path = raw_exomiser.path.replace("{{BATCH_ID}}", batchId)).read
+      raw_exomiser.id -> spark.read
+        .format(raw_exomiser.format.sparkFormat)
+        .options(raw_exomiser.readoptions)
+        .load(paths = exomiserFiles.map(_.url).toSeq: _*),
+      "file_info" -> exomiserFiles.toSeq.toDF()
     )
   }
 
@@ -26,14 +32,20 @@ class Exomiser(batchId: String)(implicit configuration: Configuration) extends E
                                lastRunDateTime: LocalDateTime,
                                currentRunDateTime: LocalDateTime)(implicit spark: SparkSession): DataFrame = {
     import spark.implicits._
-    data(raw_exomiser.id)
+    val fileInfo = data("file_info").select("url", "aliquot_id")
+    val withAliquotId = data(raw_exomiser.id)
+      .withColumn("url", input_file_name())
+      .join(fileInfo, Seq("url"))
+      .drop("url")
+
+    withAliquotId
       .select($"CONTIG" as "chromosome",
-        castLong("START") as "start", // TODO: Besoin de faire + 1 à start et end ?
+        castLong("START") as "start",
         castLong("END") as "end",
         $"REF" as "reference",
         $"ALT" as "alternate",
         $"ID" as "id",
-        split(substring_index(input_file_name(), "/", -1), "\\.")(0) as "aliquot_id",
+        $"aliquot_id",
         castInt("#RANK") as "rank",
         $"GENE_SYMBOL" as "gene_symbol",
         castLong("ENTREZ_GENE_ID") as "entrez_gene_id",
