bdc29625700ce0eb84c49e6141956955e07f1f00
Ethienne Roy <eroy@ferlab.bio>
2024-02-05 11:40:16 -0500

    Revert "feat: clin-2530 fixed broken tests"
    This reverts commit 18e9d05bba58f14313a7e5276ac2a4e409ed1024.

--- src/main/scala/bio/ferlab/clin/etl/model/normalized/NormalizedConsequences.scala
+++ src/main/scala/bio/ferlab/clin/etl/model/normalized/NormalizedConsequences.scala
@@ -4,8 +4,6 @@
  */
 package bio.ferlab.clin.etl.model.normalized
 
-import bio.ferlab.clin.etl.model.raw.{SOMATIC_EXON, SOMATIC_INTRON}
-
 import java.sql.Timestamp
 
 
@@ -23,17 +21,17 @@ case class NormalizedConsequences(`chromosome`: String = "1",
                                   `ensembl_transcript_id`: String = "ENST00000335137",
                                   `ensembl_regulatory_id`: Option[String] = None,
                                   `feature_type`: String = "Transcript",
-                                  `strand`: String = "1",
+                                  `strand`: Int = 1,
                                   `biotype`: String = "protein_coding",
                                   `variant_class`: String = "SNV",
-                                  `exon`: SOMATIC_EXON = SOMATIC_EXON(),
-                                  `intron`: SOMATIC_INTRON = SOMATIC_INTRON(),
+                                  `exon`: EXON = EXON(),
+                                  `intron`: INTRON = INTRON(),
                                   `hgvsc`: String = "ENST00000335137.4:c.807T>C",
                                   `hgvsp`: String = "ENSP00000334393.3:p.Ser269=",
                                   `hgvsg`: String = "chr1:g.69897T>C",
-                                  `cds_position`: String = "807",
-                                  `cdna_position`: String = "843",
-                                  `protein_position`: String = "269",
+                                  `cds_position`: Int = 807,
+                                  `cdna_position`: Int = 843,
+                                  `protein_position`: Int = 269,
                                   `amino_acids`: AMINO_ACIDS = AMINO_ACIDS(),
                                   `codons`: CODONS = CODONS(),
                                   `original_canonical`: Boolean = true,
